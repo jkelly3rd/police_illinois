89129d0fd0c0620d3f7879de4a21564bf5391248
John Kelly <jkelly3rd@gmail.com>
2024-06-03 11:13:01 -0400

    Update readme and script

--- README.md
+++ README.md
@@ -1,35 +1,27 @@
 # Illinois Officer Data Processing
 
-This script processes data obtained from the state of Illinois that includes personnel information, license and certification information, and employment history for all officers certified in the state. It performs several operations to clean, standardize, and reformat the data. The original data is preserved in a csv format for reference, and a standardized index is created for further analysis.
+These data were obtained under the state open records law from the [Illinois Law Enforcement Training and Standards Board](https://www.ptb.illinois.gov). 
+
+The data released includes personnel information, license and certification information, and employment history for all officers certified in the state going back to the 1960s. Our processing performs several operations to clean, standardize, and reformat the data into a work history index file that is consistent with other states' data obtained as part of this tracking project. The original data is preserved in CSV format for reference.
 
 ## R Packages Used
 
-- tidyverse: For data manipulation and visualization
-- lubridate: For handling date-time data
-- janitor: For cleaning data and managing the workspace
+- `tidyverse`: For data manipulation and visualization
+- `lubridate`: For handling date-time data
+- `janitor`: For cleaning data and managing the workspace
 
-## Original Data
+## Data Files
 
-The raw data files are expected to be in CSV format and located in the `data/source/` directory. The files include:
+The Training and Standards Board provided five data files in response to a state public records request:
 
-- Individuals.csv
-- Certificates.csv
-- Employments.csv
-- Employers.csv
-- Decertifications.csv
+1. `Individuals.csv`: Contains personnel identifying data for law enforcement officers
+2. `Certificates.csv`: Contains certification data for law enforcement officers
+3. `Employments.csv`: Contains employment history data for all officers
+4. `Employers.csv`: Contains employer / police agency data for the state
+5. `Decertifications.csv`: Contains decertification data for officers
 
-## Output
 
-The processed data is saved in the `data/processed/` directory. The output files include:
-
-- il-2023-index.csv
-- il-2023-original-officers.csv
-- il-2023-original-licenses.csv
-- il-2023-original-employment.csv
-- il-2023-original-agencies.csv
-- il-2023-original-decertifications.csv
-
-## Processing Steps
+## Data Cleaning and Processing
 
 - A template dataframe for the officers index is created to ensure the final dataframe has the correct structure.
 - Relevant date columns are converted to the YYYY-MM-DD format to be consistent with other date fields throughout the project files from other states.
@@ -38,9 +30,16 @@ The processed data is saved in the `data/processed/` directory. The output files
 - Decertification offense date and decertification reason are added to the index file as a flag indicating that researchers can reference more detailed information about decertification in the decertification file. 
 - Processed index plus a series of CSV files with the original data obtained from Illinois are also exported.
 
-These files should be placed in a `data/source/` directory in the same location as the script.
+## Output
 
-The script will output processed data to the `data/processed/` directory.
+The script generates six CSV files:
+
+1. `il-2023-original-officers.csv`: Contains personal information data in csv format with all original data and fields provided by the state
+2. `il-2023-original-licenses.csv`: Contains certification data in csv format with all original data and fields provided by the state
+3. `il-2023-original-employment.csv`: Contains employment history data in csv format with all original data and fields provided by the state
+4. `il-2023-original-agencies.csv`: Contains employer data in csv format with all original data and fields provided by the state
+5. `il-2023-original-decertifications.csv`: Contains decertification data in csv format with all original data and fields provided by the state
+6. `il-2023-index.csv`: Contains a standardized index of officers, with several additional fields indicating the current status of an officer's license, including if it is revoked or suspended and the reason. These final fields are snapshots in time, as of the data provided in October 2022.
 
 The output files are stored in the `data/processed/` directory.
 
--- scripts/il_certification_processing.R
+++ scripts/il_certification_processing.R
@@ -1,18 +1,17 @@
-# Import required libraries
+# Load required libraries
 library(tidyverse)
 library(lubridate)
 library(janitor)
 library(stringi)
 
-# Clear the workspace to avoid conflicts with existing variables
+# Clear the workspace
 rm(list = ls())
 
-# Define the root directory and the output directory
+# Define root directory and output directory
 root_dir = getwd()
 output_dir = "data/processed/"
 
-# Files originally delivered by state of Florida as an accdb and manually exported to csv
-# Define the paths to the input/raw csv files
+# Define the paths to the input/raw csv files from state of Illinois
 persons_file = "data/source/Individuals.csv"
 licenses_file = "data/source/Certificates.csv"
 employment_file = "data/source/Employments.csv"
@@ -27,7 +26,8 @@ il_original_employment = "data/processed/il-2023-original-employment.csv"
 il_original_agencies = "data/processed/il-2023-original-agencies.csv"
 il_original_decertifications = "data/processed/il-2023-original-decertifications.csv"
 
-# Create a template dataframe for the officers index. This will be used to ensure that the final dataframe has the correct structure.
+# Create a template dataframe for the officers index.
+# This will be used to ensure that the final dataframe has the correct structure.
 template_index <- data.frame("person_nbr" = character(0),
                              "full_name" = character(0),
                              "first_name" = character(0),
@@ -43,7 +43,8 @@ template_index <- data.frame("person_nbr" = character(0),
                              "end_date" = as.Date(character(0))
 )
 
-# Load each of the raw csv files with all columns defaulted to column type col_character
+# Load each of the raw CSV files with all columns defaulted to column type col_character
+# Rename all columns to proper format and consistency with other states and index template
 person_data <- read_csv(persons_file, col_types = cols(.default = col_character()))
 colnames(person_data) <- c("person_nbr", "last_name", "first_name","middle_name","suffix","year_of_birth","certified")
 #Repeat for employment, agencies, licenses, and decertifications
@@ -56,7 +57,7 @@ colnames(licenses) <- c("license_type","person_nbr","license_nbr","year_certifie
 decertifications <- read_csv(decertifications_file, col_types = cols(.default = col_character()))
 colnames(decertifications) <- c("officer_name","agency","person_nbr","year_of_birth","decertification_offense_date","decertification_reason")
 
-# Convert relevant date columns to the YYYY-MM-DD format we are using in all indexes; using the ymd function from the lubridate package.
+# Convert relevant date columns to the YYYY-MM-DD format we are using in all indexes; using ymd function from lubridate
 employment$start_date <- mdy(employment$start_date) %>% as.Date()
 employment$end_date <- mdy(employment$end_date) %>% as.Date()
 decertifications$decertification_offense_date <- mdy(decertifications$decertification_offense_date) %>% as.Date()
@@ -65,12 +66,11 @@ decertifications$decertification_offense_date <- mdy(decertifications$decertific
 illinois_roster <- left_join(employment %>% select("person_nbr","agency_nbr","active","start_date","end_date","type","rank"),
                             agencies %>% select ("agency_nbr","agency"), 
                             by = "agency_nbr")
-
 illinois_roster <- left_join(illinois_roster,
                             person_data, 
                             by = "person_nbr")
 
-# convert year of birth columns to number
+# Convert year of birth columns to number
 illinois_roster$year_of_birth <- as.numeric(illinois_roster$year_of_birth)
 
 # Change all the name columns to title case
@@ -78,12 +78,12 @@ illinois_roster$first_name <- illinois_roster$first_name %>% str_to_title()
 illinois_roster$middle_name <- illinois_roster$middle_name %>% str_to_title()
 illinois_roster$last_name <- illinois_roster$last_name %>% str_to_title()
 
-# If suffix == "Null" then change to NA
+# If suffix == "Null" then change to NA before processing name cleanup
 illinois_roster$suffix <- ifelse(illinois_roster$suffix == "NULL", NA, illinois_roster$suffix)
-# Remove from suffix anything that is not a letter
+# Remove from suffix anything that is not a letter; only after research/testing of all combinations
 illinois_roster$suffix <- illinois_roster$suffix %>% str_remove_all("[^a-zA-Z]")
 
-# Create full name field
+# Create a new full name field
 illinois_roster$full_name <- paste(illinois_roster$first_name, ifelse(is.na(illinois_roster$middle_name),"",illinois_roster$middle_name), illinois_roster$last_name, ifelse(is.na(illinois_roster$suffix),"",illinois_roster$suffix), sep=" ")
 illinois_roster$full_name <- illinois_roster$full_name %>% str_squish()
 
@@ -98,7 +98,7 @@ illinois_roster <- illinois_roster %>% select(-agency_nbr)
 il_officers_index <- bind_rows(template_index,illinois_roster)
 il_officers_index <- il_officers_index %>% arrange(desc(person_nbr), desc(start_date), full_name)
 
-# Export to csv
+# Export csv of work history index for project
 il_officers_index %>% write_csv(il_index)
 
 # Export a series of csv files with the original data obtained from Illinois
